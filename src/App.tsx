34f7fbfc613b8dbab860aeffb7d409f3c6d07d6e
Brody <brodypen7987@gmail.com>
2024-08-22 17:09:22 -0500

    Hero image

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import './App.css'
-import OfferCards from './components/OfferCards'
-import SectionAnnoucement from './components/SectionAnnoucement'
+import "./App.css";
+import NavBar from "./components/NavBar";
+import OfferCards from "./components/OfferCards";
+import SectionAnnoucement from "./components/SectionAnnoucement";
+import HeroImage from "./sections/HeroImage";
 
 function App() {
-
   return (
-    <>
-      <div>
-        <SectionAnnoucement text="hey" color="red" Icon="red"/>
-        <OfferCards/>
+    <div className="m-0">
+      <NavBar />
+      <div className="m-0">
+        <div className="h-16 w-screen bg-red-500"></div>
+        <HeroImage />
+        <SectionAnnoucement text="hey" color="red" Icon="red" />
+        <OfferCards />
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
