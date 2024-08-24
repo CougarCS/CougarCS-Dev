57866bcdb7a7775075ebf406d497ad24e3c69112
Brandon Chung <chung.brandon123@gmail.com>
2024-08-24 18:54:46 -0500

    Created meet officer section

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,26 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import OfferCards from "./components/OfferCards";
 import SectionAnnoucement from "./components/SectionAnnoucement";
+import Affiliates from "./sections/Affiliates";
+import Branches from "./sections/Branches";
 import HeroImage from "./sections/HeroImage";
+import MeetOfficers from "./sections/MeetOfficers";
 
 function App() {
-  return (
-    <div className="m-0">
-      <NavBar />
-      <div className="m-0">
-        <div className="h-16 w-screen bg-red-500"></div>
-        <HeroImage />
-        <SectionAnnoucement text="hey" color="red" Icon="red" />
-        <OfferCards />
-      </div>
-    </div>
-  );
+	return (
+		<div className="m-0">
+			<NavBar />
+			<div className="m-0">
+				<div className="h-16 w-screen bg-red-500"></div>
+				<HeroImage />
+				<SectionAnnoucement text="hey" color="red" Icon="red" />
+				<OfferCards />
+				<Branches />
+				<Affiliates />
+				<MeetOfficers />
+			</div>
+		</div>
+	);
 }
 
 export default App;
