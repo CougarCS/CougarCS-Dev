d19318f5bc669611ace8295033dccd77fc1a3b4b
Brody <brodypen7987@gmail.com>
2024-08-25 14:38:15 -0500

    add: husky prettier

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,21 @@ import HeroImage from "./sections/HeroImage";
 import MeetOfficers from "./sections/MeetOfficers";
 
 function App() {
-	return (
-		<div className="m-0">
-			<NavBar />
-			<div className="m-0">
-				<div className="h-16 w-screen bg-red-500"></div>
-				<HeroImage />
-        <ClubStats/>
-				<SectionAnnoucement text="hey" color="red" Icon="red" />
-				<OfferCards />
-				<Branches />
-				<Affiliates />
-				<MeetOfficers />
-			</div>
-		</div>
-	);
+  return (
+    <div className="m-0">
+      <NavBar />
+      <div className="m-0">
+        <div className="h-16 w-screen bg-red-500"></div>
+        <HeroImage />
+        <ClubStats />
+        <SectionAnnoucement text="hey" color="red" Icon="red" />
+        <OfferCards />
+        <Branches />
+        <Affiliates />
+        <MeetOfficers />
+      </div>
+    </div>
+  );
 }
 
 export default App;
