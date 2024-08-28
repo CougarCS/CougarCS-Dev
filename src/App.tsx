28d9aba2896594c374b3b7fab4da12ab2837e1bb
Brandon Chung <chung.brandon123@gmail.com>
2024-08-28 17:27:21 -0500

    Create branches section for web version

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import OfferCards from "./components/OfferCards";
 import SectionAnnoucement from "./components/SectionAnnoucement";
 import AboutUs from "./sections/AboutUs";
 import Affiliates from "./sections/Affiliates";
-import Branches from "./sections/Branches";
+import MobileBranches from "./components/mobile/Branches";
 import ClubStats from "./sections/ClubStats";
 import HeroImage from "./sections/HeroImage";
 import MeetOfficers from "./sections/MeetOfficers";
+import Branches from "./sections/Branches";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <AboutUs />
         <OfferCards />
         <Branches />
+        <MobileBranches />
         <Affiliates />
         <MeetOfficers />
       </div>
