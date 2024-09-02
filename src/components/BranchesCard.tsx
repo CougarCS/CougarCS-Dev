9e9460654a650e436e09d516266135d02083cbf1
Brandon Chung <chung.brandon123@gmail.com>
2024-09-02 16:23:25 -0500

    Fix responsive css for branches section

--- src/components/BranchesCard.tsx
+++ src/components/BranchesCard.tsx
@@ -6,7 +6,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import type { Activity } from "../util/branches";
+import type { Activity } from "../displayData/branches";
 
 type BranchesCardProps = {
   name: string;
@@ -37,21 +37,21 @@ const BranchesCard: React.FC<BranchesCardProps> = ({
 }: BranchesCardProps) => {
   return (
     <Card
-      className={`border-b-4 ${bottomBorderColor} rounded-b-none shadow-md lg:max-w-[25rem]`}
+      className={`border-b-4 ${bottomBorderColor} rounded-b-none shadow-md`}
     >
-      <CardHeader className="text-left p-8">
+      <CardHeader className="text-left p-8 lg:p-7 2xl:p-8">
         <span className={`font-bold mb-3 ${textColor}`}>Overview</span>
 
         <CardTitle className="text-cardPrimary font-semibold text-2xl mb-3">
           {name}
         </CardTitle>
 
-        <CardDescription className="text-sm text-cardSecondary font-medium text-left sm:text-base sm:font-normal">
+        <CardDescription className="text-sm text-cardSecondary font-medium text-pretty lg:text-[15px]/6 lg:font-normal 2xl:text-base">
           {description}
         </CardDescription>
       </CardHeader>
 
-      <CardContent className="p-8 pt-0">
+      <CardContent className="px-8 pb-8 lg:px-7 lg:pb-7 2xl:px-8 2xl:pb-8 pt-0">
         <div className="flex flex-col gap-5">
           {activities.map(({ svgPath, title }) => (
             <div className="flex items-center gap-[10px]" key={title}>
@@ -69,7 +69,7 @@ const BranchesCard: React.FC<BranchesCardProps> = ({
         </div>
       </CardContent>
 
-      <CardFooter className="p-8 pt-0">
+      <CardFooter className="px-8 pb-8 lg:px-7 lg:pb-7 2xl:px-8 2xl:pb-8  pt-0">
         <a
           href={learnMoreHref}
           target="_blank"
@@ -77,7 +77,7 @@ const BranchesCard: React.FC<BranchesCardProps> = ({
           className="flex items-center gap-[10px] mb-1"
         >
           <p
-            className={`${textColor} ${hoverBorderColor} border-b-[1.5px] border-b-transparent text-sm font-semibold lg:text-base`}
+            className={`${textColor} ${hoverBorderColor} border-b-[1.5px] border-b-transparent text-sm font-semibold lg:text-[15px]`}
           >
             {learnMoreText}
           </p>
